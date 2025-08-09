d6e0bc07bfd7ee061c73e685faf06e0a96500b9e
igrshrn <igrshrn@yandex.ru>
2025-08-09 23:52:10 +0300

    Completed 3

---
+++ src/main/java/ru/edu/leetcode/medium/task_3/Solution.java
@@ -0,0 +1,25 @@
+package ru.edu.leetcode.medium.task_3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char current = s.charAt(right);
+
+            while (set.contains(current)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(current);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
