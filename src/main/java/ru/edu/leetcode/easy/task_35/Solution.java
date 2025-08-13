972acafd6522ebb6acf6f84aeaf2e461b494a05a
igrshrn <igrshrn@yandex.ru>
2025-08-13 21:26:49 +0300

    Completed 35

---
+++ src/main/java/ru/edu/leetcode/easy/task_35/Solution.java
@@ -0,0 +1,21 @@
+package ru.edu.leetcode.easy.task_35;
+
+/** 35. Search Insert Position https://leetcode.com/problems/search-insert-position/description/ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
